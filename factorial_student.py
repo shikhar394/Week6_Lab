683fdf1d4d2947716910d9adc1bf64a90c20a340
mebiusvista <mebiusvista@gmail.com>
2017-03-16 13:28:38 +0800

    Add files via upload

---
+++ factorial_student.py
@@ -0,0 +1,9 @@
+def factorial(num):
+    return
+
+def main():
+    number = int(input("Enter a nonnegative integer: "))
+    fact = factorial(number)
+    print("The factorial of", number, "is", fact)
+        
+main()
---
+++ student_info.py
@@ -0,0 +1,29 @@
+class Student:
+    def __init__(self, name = "", class_of = 0, major = ""):
+        self.name = name
+        self.class_of = class_of
+        self.major = major
+        
+    def set_name(self, name):
+        self.name = name
+        
+    def get_name(self):
+        return self.name            
+    
+    def set_class_of(self, class_of):
+        pass
+    
+    def get_class_of(self):
+        pass
+    
+    def set_major(self, major):
+        pass
+    
+    def get_major(self):
+        pass
+        
+s = Student(name = "James")
+"""
+test your case below, call the methods you created to get desired information
+"""
+
